46d2a7644d63af10fdcda563cdc18e2a0e29ba3d
Ethan Guo <ethguo@gmail.com>
2017-09-23 04:15:52 -0400

    Webpack Example App

--- source/app.js
+++ source/app.js
@@ -3,8 +3,8 @@
 
   if (!window.addEventListener) return // Check for IE9+
 
-  var options = INSTALL_OPTIONS
-  var element
+  let options = INSTALL_OPTIONS
+  let element
 
   // updateElement runs every time the options are updated.
   // Most of your code will end up inside this function.
@@ -18,7 +18,7 @@
 
   // INSTALL_SCOPE is an object that is used to handle option changes without refreshing the page.
   window.INSTALL_SCOPE = {
-    setOptions: function setOptions (nextOptions) {
+    setOptions (nextOptions) {
       options = nextOptions
 
       updateElement()
