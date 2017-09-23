da9cd531a11776a01759a88b15baa9bf79bf8d29
Ethan Guo <ethguo@gmail.com>
2017-09-23 08:15:06 -0400

    Started adding new HTML

--- src/app.js
+++ src/app.js
@@ -1,34 +1,44 @@
 (function () {
-  'use strict'
+  'use strict';
 
-  if (!window.addEventListener) return // Check for IE9+
+  if (!window.addEventListener) return; // Check for IE9+
 
-  let options = INSTALL_OPTIONS
-  let element
+  let options = INSTALL_OPTIONS;
+  let element;
+
+  let controlsHTML = `
+    <label class="swilter-switch">
+      <input type="checkbox" checked>
+      <span class="swilter-switch-slider"></span>
+
+      Swilter
+    </label>
+  `;
 
   // updateElement runs every time the options are updated.
   // Most of your code will end up inside this function.
   function updateElement () {
-    element = INSTALL.createElement(options.location, element)
+    element = INSTALL.createElement(options.location, element);
 
     // Set the app attribute to your app's dash-delimited alias.
-    element.setAttribute('app', 'swilter')
-    element.innerHTML = options.message
+    element.setAttribute('app', 'swilter');
+
+    element.innerHTML = controlsHTML;
   }
 
   // INSTALL_SCOPE is an object that is used to handle option changes without refreshing the page.
   window.INSTALL_SCOPE = {
     setOptions (nextOptions) {
-      options = nextOptions
+      options = nextOptions;
 
-      updateElement()
+      updateElement();
     }
   }
 
   // This code ensures that the app doesn't run before the page is loaded.
   if (document.readyState === 'loading') {
-    document.addEventListener('DOMContentLoaded', updateElement)
+    document.addEventListener('DOMContentLoaded', updateElement);
   } else {
-    updateElement()
+    updateElement();
   }
-}())
+}();)
