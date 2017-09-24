d2a7bc467497171eb892d67e513bec823643e9e2
coolcom200 <leonfattakhov@yahoo.com>
2017-09-24 04:19:52 -0400

    Reformatted code

--- backend.js
+++ backend.js
@@ -1,40 +1,14 @@
 "use strict";
-var unirest = require('unirest');
 let fetch = require("node-fetch");
 function IsSwear(string) {
-    var badwordRegex = /(\b(ur mom|(yo)?u suck|8={3,}D|nigg[aeu][rh]?|ass|(ass ?|a|a-)hole|fag(got)?|daf[au][qk]|(brain|mother|mutha)?fuc?k+(a|ing?|e?(r|d)| off+| y(ou|e)(rself)?|u+|tard)?|shit?(t?er|s|head)?|you scum|dickhead|pedo|whore|cunt|cocksucker|ejaculated?|jerk off|cummies|butthurt|queef|(private|pussy) show|pussy|lesbo|bitche?s?|hoe|(eat|suck)\b.{0,20}\b dick|dee[sz]e? nut[sz])s?\b)/;
-    var re = new RegExp(badwordRegex);
+    let badwordRegex = /(\b(ur mom|(yo)?u suck|8={3,}D|nigg[aeu][rh]?|ass|(ass ?|a|a-)hole|fag(got)?|daf[au][qk]|(brain|mother|mutha)?fuc?k+(a|ing?|e?(r|d)| off+| y(ou|e)(rself)?|u+|tard)?|shit?(t?er|s|head)?|you scum|dickhead|pedo|whore|cunt|cocksucker|ejaculated?|jerk off|cummies|butthurt|queef|(private|pussy) show|pussy|lesbo|bitche?s?|hoe|(eat|suck)\b.{0,20}\b dick|dee[sz]e? nut[sz])s?\b)/;
+    let re = new RegExp(badwordRegex);
     return re.exec(string) !== null;
 }
-
-// function IsOffensive(text) {
-//     var sentiment;
-//     var request = unirest.post("https://eastus2.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment")
-//         .headers({"Content-Type": "application/json", "Ocp-Apim-Subscription-Key": "e6ed6519b56e44a9bd743bd236f4a31c"})
-//         .send({
-//             "documents": [
-//                 {
-//                     "language": "en",
-//                     "id": "string",
-//                     "text": text
-//                 }
-//             ]
-//         })
-//         .end(function (response) {
-//             // console.log(response)
-//             var data = response.body;
-//             sentiment = data
-//             return sentiment["documents"][0]["score"];
-//         });
-//
-//
-// }
-
-
 function IsOffensive(text) {
 
-    var resultingStuff;
-    var data = {
+    let resultingStuff;
+    let data = {
         "documents": [
             {
                 "language": "en",
@@ -50,40 +24,12 @@ function IsOffensive(text) {
             headers: {"Content-Type": "application/json", "Ocp-Apim-Subscription-Key": "b0df8262f41e49e98c4d043237ae5c65"}})
             .then(res => res.json())
             .then(json => {
-                console.log(json)
-                var sentiment = json["documents"][0]["score"];
+                console.log(json);
+                let sentiment = json["documents"][0]["score"];
                 console.log("Me"+sentiment);
-
-                if (sentiment < 0.03) {
-                    console.log("HIDE ==>O");
-                    resultingStuff = true;
-                    // Thus the message is negative and contains anger
-
-                }
-                else {
-                    console.log("PASS");
-                    resultingStuff = false;
-
-
-                }
+                resultingStuff = sentiment < 0.03;
                 return resultingStuff;
             });
 
 }
 
-// For testing only
-function main(text) {
-
-    (IsSwear(text));
-    console.log(IsOffensive(text));
-    if (IsSwear(text)) {
-        // console.log("HIDE ==> S")
-    }
-    else {
-
-        // console.log("HIDE ==> O")
-    }
-}
-
-
-main("Good job... not");
--- giphy.js
+++ giphy.js
@@ -1,18 +1,17 @@
-var fetch = require('node-fetch');
+let fetch = require('node-fetch');
 
-function getGIPHY(keywords) {
-    searchQ = keywords[Math.floor(Math.random() * keywords.length) - 1]
-    console.log(searchQ)
+function getGIPHY(keywords=["cats","dogs","bears","love"]) {
+    searchQ = keywords[Math.floor(Math.random() * keywords.length) - 1];
+    console.log(searchQ);
     return fetch("https://api.giphy.com/v1/gifs/search?api_key=GBov9dSMPUy2DLg9DMO0miWJlBP3sEo4&q=" + searchQ + "&limit=1&offset=0&rating=G&lang=en")
         .then(res => {
             return res.json();
         })
         .then(json => {
-            // console.log(json);
             return json["data"][0]["embed_url"];
         });
 }
 
-getGIPHY(["toby", "nojob", "nolife", "money", "love"]).then(url => {
+getGIPHY().then(url => {
     console.log("url " + url);
 });
